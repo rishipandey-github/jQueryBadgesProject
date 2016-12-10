ee63a273491dd24d55be80113f88eb146d00ddd1
Rishi Pandey <rishipandey888@gmail.com>
2016-12-10 23:58:47 +1300

    Updated logic for adding badges.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,55 +1,43 @@
 $(function() {
-
   $.ajax({
     url: 'https://www.codeschool.com/users/rishipandey888.json',
     dataType: 'jsonp',
-    success: function(response) {     
+    success: function(response) {
       var courses = response.courses.completed;
       
       $.each(courses, function( key, value ) {
+
+        var outer = jQuery('<div/>', {
+          class: 'course'
+        }).appendTo('#badges');
+
+        addHeader(outer, value.title);
+        addImage(outer, value.badge);
+        addCourseLink(outer, value.url);
         
-        d = document.createElement('div');
-        $(d).addClass("course")        
-        .appendTo($("#badges"))
-        
-        addHeader(d, value.title);
-        addImage(d, value.badge);        
-        addLink(d, value.url);
       });
     }
   });
 
-  function addHeader(parent, headerText) {
-    h = document.createElement('h3');
-    $(h).html(headerText);
-        
-    parent.append(h);
+  function addHeader(parent, titleValue) {
+    jQuery('<h3/>', {
+      html: titleValue
+    }).appendTo(parent);
   }
 
   function addImage(parent, imageUrl) {
-    img = document.createElement('img');        
-    a = document.createAttribute("src");
-
-    a.value = imageUrl;
-    img.setAttributeNode(a);
-
-    parent.append(img);
+    jQuery('<img/>', {
+      src: imageUrl
+    }).appendTo(parent);
   }
 
-  function addLink(parent, badgeUrl) {
-    badgeLink = document.createElement('a');
-
-    $(badgeLink).html('See Course');
-    $(badgeLink).addClass("btn btn-primary");
-    
-    badgeHref = document.createAttribute("href");
-    badgeHref.value = badgeUrl;
-    badgeLink.setAttributeNode(badgeHref);
-
-    targetAttribute = document.createAttribute("target");
-    targetAttribute.value = "_blank";
-    badgeLink.setAttributeNode(targetAttribute);
-
-    parent.append(badgeLink);
+  function addCourseLink(parent, courseLink) {
+    jQuery('<a/>', {
+      class: 'btn btn-primary',
+      href: courseLink,
+      target: '_blank',
+      text: 'See Course'
+    }).appendTo(parent);
   }
+
 });
